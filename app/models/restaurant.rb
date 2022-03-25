026a691fcd9ff3a2fbd63d3e589fe8a7f56ba682
jrkinouani <kinouani@gmail.com>
2022-03-25 21:12:36 +0100

    delete amount

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,3 @@
 class Restaurant < ApplicationRecord
-  attr_reader :amount
-  def initialize(amount)
-    super()
-    @amount = amount
-  end
-
-  def amount
-    price + host + taste + decor
-  end
+    
 end
