9d69d6d39e0ab15e7337dd0c068db1a50268f1f3
jrkinouani <kinouani@gmail.com>
2022-04-06 14:06:49 +0200

    change aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,7 +2,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      :aws_access_key_id => ENV['aAWS_ACCESS_KEY_ID'],
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :region    =>'eu-west-3',
       :host => 's3.amazonaws.com'
